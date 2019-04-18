a88985fc9166e1dc1961e13d9540273d3fc61e07
sungmin414 <sungmin3231@gmail.com>
2019-04-18 18:01:20 +0900

    signup view에서 중복 render()부분 제거

--- app/members/views.py
+++ app/members/views.py
@@ -34,36 +34,24 @@ def logout_view(request):
 
 
 def signup_view(request):
+    context = {
+        'form': SignupForm,
+    }
     if request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
         if User.objects.filter(username=username).exists():
-            form = SignupForm()
-            context = {
-                'form': form,
-                'error': f'({username})은 이미 사용중입니다'
-            }
-            return render(request, 'members/signup.html', context)
-        if password1 != password2:
-            form = SignupForm()
-            context = {
-                'form': form,
-                'error': '비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다'
-            }
-            return render(request, 'members/signup.html', context)
-        
-        # create_user메서드는 create와 달리 자동으로 password해싱을 해줌
-        user = User.objects.create_user(
-            username=username,
-            password=password1,
-        )
-        login(request, user)
-        return redirect('posts:post-list')
-    else:
-        form = SignupForm()
-        context = {
-            'form': form
-        }
-        return render(request, 'members/signup.html', context)
+            context['error'] = f'({username})은 이미 사용중입니다'
+        elif password1 != password2:
+            context['error'] = '비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다'
+        else:
+            # create_user메서드는 create와 달리 자동으로 password해싱을 해줌
+            user = User.objects.create_user(
+                username=username,
+                password=password1,
+            )
+            login(request, user)
+            return redirect('posts:post-list')
+    return render(request, 'members/signup.html', context)

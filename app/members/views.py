99c4d70941b5bcf606823776221da5f39bdc262d
sungmin414 <sungmin3231@gmail.com>
2019-04-18 17:27:38 +0900

    username, password1, password2가 유효할 경우의 회원가입 구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import LoginForm, SignupForm
@@ -33,7 +35,22 @@ def logout_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username).exists():
+            return HttpResponse(f'사용자명({username})은 이미 사용중입니다')
+        if password1 != password2:
+            return HttpResponse('비밀번호와 비밀번호 확인란의 값이 일치하지 않습니다')
+
+        # create_user메서드는 create와 달리 자동으로 password해싱을 해줌
+        user = User.objects.create_user(
+            username=username,
+            password=password1,
+        )
+        login(request, user)
+        return redirect('posts:post-list')
     else:
         form = SignupForm()
         context = {

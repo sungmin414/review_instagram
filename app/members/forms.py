7326a59bce4f8c0cd0130f9d5a00d9f8f2a33a54
sungmin414 <sungmin3231@gmail.com>
2019-04-19 00:51:15 +0900

    Bootstrap에 맞게 CSS동적 추가

--- app/members/forms.py
+++ app/members/forms.py
@@ -25,6 +25,7 @@ class LoginForm(forms.Form):
 
 class SignupForm(forms.Form):
     username = forms.CharField(
+        label='사용자명',
         widget=forms.TextInput(
             attrs={
                 'class': 'form-control',
@@ -32,6 +33,7 @@ class SignupForm(forms.Form):
         )
     )
     password1 = forms.CharField(
+        label='비밀번호',
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
@@ -39,6 +41,7 @@ class SignupForm(forms.Form):
         )
     )
     password2 = forms.CharField(
+        label='비밀번호확인',
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control',
@@ -50,12 +53,14 @@ class SignupForm(forms.Form):
         # username이 유일한지 검사
         data = self.cleaned_data['username']
         if User.objects.filter(username=data).exists():
+            self.fields['username'].widget.attrs['class'] += ' is-invalid'
             raise forms.ValidationError('이미 사용중 사용자명입니다')
 
-    def clean(self):
-        # password1, password2가 일치하는지 검사
-        super().clean()
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
         if password1 != password2:
+            self.fields['password1'].widget.attrs['class'] += ' is-invalid'
+            self.fields['password2'].widget.attrs['class'] += ' is-invalid'
             raise forms.ValidationError('비밀번호와 비밀번호 확인란의 값이 다릅니다')
+        return password2
